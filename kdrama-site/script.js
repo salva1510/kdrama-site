1141adced328ffcdcc9c19287f7880fbed286f32
salva1510 <hjpoffice2@gmail.com>
2026-01-09 15:26:31 +0800

    Update script.js

--- kdrama-site/script.js
+++ kdrama-site/script.js
@@ -1,18 +1,30 @@
-const dramas = [
-  { title: "Crash Landing on You", year: "2019" },
-  { title: "Goblin", year: "2016" },
-  { title: "Itaewon Class", year: "2020" }
+const API_KEY = "4c918f6f";
+
+const dramaTitles = [
+  "Crash Landing on You",
+  "Goblin",
+  "Itaewon Class"
 ];
 
 const container = document.getElementById("dramas");
 
-if (container) {
-  dramas.forEach(d => {
-    const div = document.createElement("div");
-    div.className = "drama";
-    div.innerHTML = `<h3>${d.title}</h3><p>${d.year}</p>`;
-    container.appendChild(div);
-  });
+async function loadDramas() {
+  for (const title of dramaTitles) {
+    const res = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${API_KEY}`);
+    const data = await res.json();
+
+    if (data.Response === "True") {
+      const div = document.createElement("div");
+      div.className = "drama";
+      div.innerHTML = `
+        <img src="${data.Poster}" width="150"><br>
+        <h3>${data.Title}</h3>
+        <p>⭐ IMDb: ${data.imdbRating}</p>
+        <p>${data.Year}</p>
+      `;
+      container.appendChild(div);
+    }
+  }
 }
 
-
+if (container) loadDramas();
